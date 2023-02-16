b50dcf1f54d4778ca4a8ae51d13b44df5fc95ee5
Zhang Jing <mrzleo@foxmail.com>
2023-02-16 02:53:14 +0000

    closes #1
    use rootfs from config.json directly rather than bundle/rootfs.

--- fork2container.go
+++ fork2container.go
@@ -5,90 +5,113 @@ package main
 
 /*
 #include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/un.h>
+#include <unistd.h>
 
-int
-sendfds(int s, int *fds, int fdcount) {
-	char buf[1];
-	struct iovec iov;
-	struct msghdr header;
-	struct cmsghdr *cmsg;
-	int n;
-	char cms[CMSG_SPACE(sizeof(int) * fdcount)];
+int sendfds(int s, int *fds, int fdcount)
+{
+    char buf[1];
+    struct iovec iov;
+    struct msghdr header;
+    struct cmsghdr *cmsg;
+    int n;
+    char cms[CMSG_SPACE(sizeof(int) * fdcount)];
 
-	buf[0] = 0;
-	iov.iov_base = buf;
-	iov.iov_len = 1;
+    buf[0] = 0;
+    iov.iov_base = buf;
+    iov.iov_len = 1;
 
-	memset(&header, 0, sizeof header);
-	header.msg_iov = &iov;
-	header.msg_iovlen = 1;
-	header.msg_control = (caddr_t)cms;
-	header.msg_controllen = CMSG_LEN(sizeof(int) * fdcount);
+    memset(&header, 0, sizeof header);
+    header.msg_iov = &iov;
+    header.msg_iovlen = 1;
+    header.msg_control = (caddr_t)cms;
+    header.msg_controllen = CMSG_LEN(sizeof(int) * fdcount);
 
-	cmsg = CMSG_FIRSTHDR(&header);
-	cmsg->cmsg_len = CMSG_LEN(sizeof(int) * fdcount);
-	cmsg->cmsg_level = SOL_SOCKET;
-	cmsg->cmsg_type = SCM_RIGHTS;
-	memmove(CMSG_DATA(cmsg), fds, sizeof(int) * fdcount);
+    cmsg = CMSG_FIRSTHDR(&header);
+    cmsg->cmsg_len = CMSG_LEN(sizeof(int) * fdcount);
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    memmove(CMSG_DATA(cmsg), fds, sizeof(int) * fdcount);
 
-	if((n = sendmsg(s, &header, 0)) != iov.iov_len) {
-		return -1;
-	}
+    if ((n = sendmsg(s, &header, 0)) != iov.iov_len) {
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 // Send multiple FDs to the unix socket
-int
-sendMultipleFDs(char *sockPath, int chrootFD, int utsNamespaceFD, int pidNamespaceFD, int ipcNamespaceFD, int mntNamespaceFD) {
-	// Connect to server via socket.
-	int s, len, ret;
-	struct sockaddr_un remote;
+int sendMultipleFDs(
+    char *sockPath,
+    int chrootFD,
+    int utsNamespaceFD,
+    int pidNamespaceFD,
+    int ipcNamespaceFD,
+    int mntNamespaceFD)
+{
+    printf("send multiple fds\n");
+    // Connect to server via socket.
+    struct sockaddr_un_longer {
+        // unsigned char sun_len;
+        sa_family_t sun_family;
+        char sun_path[512];
+    };
 
-	if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-		return -1;
-	}
+    int s, len, ret;
+    struct sockaddr_un_longer remote = {
+        .sun_family = AF_UNIX
+    };
 
-	remote.sun_family = AF_UNIX;
-	strcpy(remote.sun_path, sockPath);
-	len = strlen(remote.sun_path) + sizeof(remote.sun_family);
-	if (connect(s, (struct sockaddr *)&remote, len) == -1) {
-		return -1;
-	}
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        printf("error when open socket\n");
+        return -1;
+    }
 
-	int fds[5];
-	fds[0] = chrootFD;
-	fds[1] = utsNamespaceFD;
-	fds[2] = pidNamespaceFD;
-	fds[3] = ipcNamespaceFD;
-	fds[4] = mntNamespaceFD;
+    // remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, sockPath);
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+	printf("remote: %s\n", (char*)&remote);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+        printf("errno is: %d\n", errno);
+        printf("error when connect socket\n");
+        return -1;
+    }
 
-	if (sendfds(s, fds, 5) == -1) {
-		return -1;
-	}
+    int fds[5];
+    fds[0] = chrootFD;
+    fds[1] = utsNamespaceFD;
+    fds[2] = pidNamespaceFD;
+    fds[3] = ipcNamespaceFD;
+    fds[4] = mntNamespaceFD;
 
-	char pid_arr[20];
-	if (read(s, pid_arr, 20) < 0) {
-		return -1;
-	}
+    if (sendfds(s, fds, 5) == -1) {
+        printf("error when send fds\n");
+        return -1;
+    }
 
-	int pid = atoi(pid_arr);
+    char pid_arr[20];
+    if (read(s, pid_arr, 20) < 0) {
+        printf("error when recived pid\n");
+        return -1;
+    }
 
-	if(close(s) == -1) {
-		return -1;
-	}
+    int pid = atoi(pid_arr);
 
-	return pid;
+    if (close(s) == -1) {
+        printf("error when close socket\n");
+        return -1;
+    }
+
+    printf("send finished\n");
+    return pid;
 }
 */
 import "C"
@@ -123,7 +146,7 @@ var fork2ContainerCommand = cli.Command{
 		cli.StringFlag{
 			Name:  "fork-socket",
 			Usage: "the relative path to the fork socket in the zygote container according to the bundle path",
-			Value: "rootfs/fork.sock",
+			Value: "fork.sock",
 		},
 	},
 	Action: func(context *cli.Context) error {
@@ -216,23 +239,30 @@ func doFork(context *cli.Context, zygoteContainerID string, targetContainerID st
 	defer mntNamespaceFd.Close()
 
 	// open the rootfs of target container
-	targetContainerBundle, _ := utils.Annotations(targetContainerState.Config.Labels)
-	targetContainerRootfs, err := securejoin.SecureJoin(targetContainerBundle, "rootfs")
-	if err != nil {
-		return err
-	}
+	// targetContainerBundle, _ := utils.Annotations(targetContainerState.Config.Labels)
+	// targetContainerRootfs, err := securejoin.SecureJoin(targetContainerBundle, "rootfs")
+	// if err != nil {
+	// 	return err
+	// }
+	targetContainerRootfs := targetContainer.Config().Rootfs
 	targetContainerRootfsFd, err := os.Open(targetContainerRootfs)
 	if err != nil {
 		return err
 	}
 	defer targetContainerRootfsFd.Close()
 
+	fmt.Printf("target rootfs=%s\n", targetContainerRootfs)
+
 	// find the path to zygote container fork socket
-	zygoteContainerBundle, _ := utils.Annotations(zygoteContainerState.Config.Labels)
-	zygoteContainerForkSocketPath, err := securejoin.SecureJoin(zygoteContainerBundle, forkSocketPath)
+	// zygoteContainerBundle, _ := utils.Annotations(zygoteContainerState.Config.Labels)
+	zygoteContainerRootfs := zygoteContainer.Config().Rootfs
+	zygoteContainerForkSocketPath, err := securejoin.SecureJoin(zygoteContainerRootfs, forkSocketPath)
+	fmt.Printf("socket path=%s\n", zygoteContainerForkSocketPath)
 	if err != nil {
 		return err
 	}
+
+	fmt.Println("get all fds")
 
 	// send the fds to the socket
 	pid, err := invokeMultipleFds(
@@ -243,12 +273,19 @@ func doFork(context *cli.Context, zygoteContainerID string, targetContainerID st
 		ipcNamespaceFd,
 		mntNamespaceFd,
 	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("send fds success")
 
 	// apply target container's cgroup to new process
 	err = (*targetrCgroupManager).Apply(pid)
 	if err != nil {
 		return err
 	}
+
+	fmt.Println("apply cgroup success")
 
 	return nil
 }
